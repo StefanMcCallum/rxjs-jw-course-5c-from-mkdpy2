e7ee1c1e790e3af719d0e6a58089738f43a69bef
StefanMcCallum <stefan.mccallum@outlook.com>
2024-07-06 13:17:21 +0100

    add "from" creation function

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { from } from 'rxjs';
 
 // Very similar to of, except takes an array argument
 // Also takes a promise argument
+// Also takes an Observable argument
 
 // from(['Alice', 'Ben', 'Charlie']).subscribe({
 //   next: (value) => console.log(value),
