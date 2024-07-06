d5a3ccd0f7691d9c027a789effec00eb4403e1eb
StefanMcCallum <stefan.mccallum@outlook.com>
2024-07-06 13:14:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-jw-course-5c-from-mkdpy2
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/StefanMcCallum/rxjs-jw-course-5c-from-mkdpy2)
---
+++ index.ts
@@ -0,0 +1,22 @@
+import { from } from 'rxjs';
+
+// Very similar to of, except takes an array argument
+// Also takes a promise argument
+
+// from(['Alice', 'Ben', 'Charlie']).subscribe({
+//   next: (value) => console.log(value),
+//   complete: () => console.log('Completed'),
+// });
+
+const somePromise = new Promise((resolve, reject) => {
+  // resolve('Resolved!');
+  reject('Rejected!');
+});
+
+const observableFromPromise$ = from(somePromise);
+
+observableFromPromise$.subscribe({
+  next: (value) => console.log(value),
+  complete: () => console.log('Completed'),
+  error: (err) => console.log('Error:', err),
+});
